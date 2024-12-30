3ba00ae94543ecb028c8b586949274a02285793d
Lukasz Bulinski <lucas.bulinski@gmail.com>
2024-12-30 14:20:44 +0100

    refactoring of encoding-decoding and signing block and verification

--- core/block_test.go
+++ core/block_test.go
@@ -1,69 +1,51 @@
 package core
 
 import (
-	"bytes"
-	"fmt"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/ukibbb/blockchain-go/crypto"
 	"github.com/ukibbb/blockchain-go/types"
 )
 
-func TestHeader_Encode_Decode(t *testing.T) {
-
+func randomBlock(height uint32) *Block {
 	h := &Header{
-		Version:   1,
-		PrevBlock: types.RandomHash(),
-		Timestamp: uint64(time.Now().UnixNano()),
-		Height:    10,
-		Nonce:     987654567,
+		Version:       1,
+		PrevBlockHash: types.RandomHash(),
+		Timestamp:     uint64(time.Now().UnixNano()),
+		Height:        height,
+		Nonce:         987654567,
 	}
-	buf := &bytes.Buffer{}
-	assert.Nil(t, h.EncodeBinary(buf))
+	data := []byte("fooo")
+	tx := Transaction{Data: data}
 
-	hDecode := &Header{}
-	assert.Nil(t, hDecode.DecodeBinary(buf))
-
-	assert.Equal(t, h, hDecode)
+	return &Block{Header: h, Transactions: []Transaction{tx}}
 
 }
 
-func TestBlock_Encode_Decode(t *testing.T) {
-	b := &Block{
-		Header: Header{
-			Version:   1,
-			PrevBlock: types.RandomHash(),
-			Timestamp: uint64(time.Now().UnixNano()),
-			Height:    10,
-			Nonce:     987654567,
-		},
-		Transactions: nil,
-	}
-	buf := &bytes.Buffer{}
-	assert.Nil(t, b.EncodeBinary(buf))
+func TestSignBlock(t *testing.T) {
+	privKey := crypto.GeneratePrivateKey()
+	b := randomBlock(0)
 
-	bDecode := &Block{}
-	assert.Nil(t, bDecode.DecodeBinary(buf))
-	assert.Equal(t, b, bDecode)
-	fmt.Printf("decoded block %v", bDecode)
+	assert.Nil(t, b.Sign(privKey))
+	assert.NotNil(t, b.Signature)
 
 }
+func TestVerifyBlock(t *testing.T) {
+	privKey := crypto.GeneratePrivateKey()
+	b := randomBlock(0)
 
-func TestBlockHash(t *testing.T) {
-	b := &Block{
-		Header: Header{
-			Version:   1,
-			PrevBlock: types.RandomHash(),
-			Timestamp: uint64(time.Now().UnixNano()),
-			Height:    10,
-			Nonce:     987654567,
-		},
-		Transactions: []Transaction{},
-	}
+	assert.Nil(t, b.Sign(privKey))
+	assert.Nil(t, b.Verify())
 
-	h := b.Hash()
-	fmt.Println(h)
-	assert.False(t, h.IsZero())
+	otherPrivKey := crypto.GeneratePrivateKey()
+	b.Validator = otherPrivKey.PublicKey()
+
+	assert.NotNil(t, b.Verify())
+
+	// alter data so this is not same block
+	b.Height = 100
+	assert.NotNil(t, b.Verify())
 
 }
