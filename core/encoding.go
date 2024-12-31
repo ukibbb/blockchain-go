12095e56e7340f1b1679732ed4b89df72ec1f053
Lukasz Bulinski <lucas.bulinski@gmail.com>
2024-12-31 18:33:49 +0100

    dont'shoot

--- core/encoding.go
+++ core/encoding.go
@@ -1,6 +1,7 @@
 package core
 
 import (
+	"crypto/elliptic"
 	"encoding/gob"
 	"io"
 )
@@ -18,6 +19,7 @@ type GobTxEncoder struct {
 }
 
 func NewGobTxEncoder(w io.Writer) *GobTxEncoder {
+	gob.Register(elliptic.P256())
 	return &GobTxEncoder{
 		w: w,
 	}
@@ -32,6 +34,7 @@ type GobTxDecoder struct {
 }
 
 func NewGobTxDecoder(r io.Reader) *GobTxDecoder {
+	gob.Register(elliptic.P256())
 	return &GobTxDecoder{
 		r: r,
 	}
