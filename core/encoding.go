f292550132e7743b78777fff9cc4f62fa681faab
Lukasz Bulinski <lucas.bulinski@gmail.com>
2025-01-01 10:40:00 +0100

    changes in encoding

--- core/encoding.go
+++ core/encoding.go
@@ -1,10 +1,14 @@
 package core
 
 import (
-	"crypto/elliptic"
 	"encoding/gob"
 	"io"
 )
+
+//
+// For now we GOB encoding is used for fast bootstrapping of the project
+// in a later phase I'm considering using Protobuffers as default encoding / decoding.
+//
 
 type Encoder[T any] interface {
 	Encode(T) error
@@ -19,7 +23,6 @@ type GobTxEncoder struct {
 }
 
 func NewGobTxEncoder(w io.Writer) *GobTxEncoder {
-	gob.Register(elliptic.P256())
 	return &GobTxEncoder{
 		w: w,
 	}
@@ -34,12 +37,39 @@ type GobTxDecoder struct {
 }
 
 func NewGobTxDecoder(r io.Reader) *GobTxDecoder {
-	gob.Register(elliptic.P256())
 	return &GobTxDecoder{
 		r: r,
 	}
 }
 
-func (d *GobTxDecoder) Decode(tx *Transaction) error {
-	return gob.NewDecoder(d.r).Decode(tx)
+func (e *GobTxDecoder) Decode(tx *Transaction) error {
+	return gob.NewDecoder(e.r).Decode(tx)
+}
+
+type GobBlockEncoder struct {
+	w io.Writer
+}
+
+func NewGobBlockEncoder(w io.Writer) *GobBlockEncoder {
+	return &GobBlockEncoder{
+		w: w,
+	}
+}
+
+func (enc *GobBlockEncoder) Encode(b *Block) error {
+	return gob.NewEncoder(enc.w).Encode(b)
+}
+
+type GobBlockDecoder struct {
+	r io.Reader
+}
+
+func NewGobBlockDecoder(r io.Reader) *GobBlockDecoder {
+	return &GobBlockDecoder{
+		r: r,
+	}
+}
+
+func (dec *GobBlockDecoder) Decode(b *Block) error {
+	return gob.NewDecoder(dec.r).Decode(b)
 }
