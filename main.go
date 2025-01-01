ae5bbc383f02ecd10b8b6e2ea7c3c4abe71fabc7
Lukasz Bulinski <lucas.bulinski@gmail.com>
2025-01-01 10:41:08 +0100

    having sth working

--- main.go
+++ main.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"bytes"
+	"math/rand"
+	"strconv"
 	"time"
 
+	"github.com/sirupsen/logrus"
+	"github.com/ukibbb/blockchain-go/core"
+	"github.com/ukibbb/blockchain-go/crypto"
 	"github.com/ukibbb/blockchain-go/network"
 )
 
@@ -15,7 +21,10 @@ func main() {
 
 	go func() {
 		for {
-			trRemote.SendMessage(trLocal.Addr(), []byte("Helloworld"))
+			// trRemote.SendMessage(trLocal.Addr(), []byte("Helloworld"))
+			if err := sendTransaction(trRemote, trLocal.Addr()); err != nil {
+				logrus.Error(err)
+			}
 			time.Sleep(time.Second)
 		}
 	}()
@@ -27,4 +36,20 @@ func main() {
 	server := network.NewServer(opts)
 	server.Start()
 
+}
+
+func sendTransaction(tr network.Transport, to network.NetAddr) error {
+	// helper function
+
+	privKey := crypto.GeneratePrivateKey()
+	data := []byte(strconv.FormatInt(int64(rand.Intn(1000)), 10))
+	tx := core.NewTransaction(data)
+	tx.Sign(privKey)
+	buf := &bytes.Buffer{}
+	if err := tx.Encode(core.NewGobTxEncoder(buf)); err != nil {
+		panic(err)
+	}
+	msg := network.NewMessage(network.MessageTypeTx, buf.Bytes())
+
+	return tr.SendMessage(to, msg.Bytes())
 }
