3afefeff5ff88bf4120d156368455119606fc77f
Lukasz Bulinski <lucas.bulinski@gmail.com>
2025-01-04 09:59:27 +0100

    block already known error

--- core/validator.go
+++ core/validator.go
@@ -1,6 +1,11 @@
 package core
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
+
+var ErrBlockKnown = errors.New("block already known")
 
 type Validator interface {
 	ValidateBlock(*Block) error
@@ -16,12 +21,18 @@ func NewBlockValidator(bc *BlockChain) *BlockValidator {
 
 func (bv *BlockValidator) ValidateBlock(b *Block) error {
 	if bv.bc.HasBlock(b.Height) {
-		return fmt.Errorf("chain already contain this block (%d) with hash (%s)", b.Height, b.Hash(BlockHasher{}))
+		// return fmt.Errorf("chain already contain this block (%d) with hash (%s)", b.Height, b.Hash(BlockHasher{}))
+		return ErrBlockKnown
 	}
 
 	// if it's not after last block
 	if b.Height != bv.bc.Height()+1 {
-		return fmt.Errorf("block (%s)to high", b.Hash(BlockHasher{}))
+		return fmt.Errorf(
+			"block (%s) with height %d is to high current high => (%d)",
+			b.Hash(BlockHasher{}),
+			b.Height,
+			bv.bc.Height(),
+		)
 	}
 
 	prevHeader, err := bv.bc.GetHeader(b.Height - 1)
