a59a223861d33c25f6553281841fdd1e73264f36
Lukasz Bulinski <lucas.bulinski@gmail.com>
2025-01-04 10:43:27 +0100

    new status get status messages

--- network/rpc.go
+++ network/rpc.go
@@ -13,13 +13,21 @@ import (
 type MessageType byte
 
 const (
-	MessageTypeTx MessageType = 0x1
-	MessageTypeBlock
+	MessageTypeTx        MessageType = 0x1
+	MessageTypeBlock     MessageType = 0x2
+	MessageTypeGetBlocks MessageType = 0x3
+	MessageTypeStatus    MessageType = 0x4
+	MessageTypeGetStatus MessageType = 0x5
 )
 
 type Message struct {
 	Header MessageType
 	Data   []byte
+}
+
+type RPC struct {
+	From    NetAddr
+	Payload io.Reader
 }
 
 func NewMessage(t MessageType, data []byte) *Message {
@@ -64,12 +72,33 @@ func DefaultRPCDecodeFunc(rpc RPC) (*DecodedMessage, error) {
 			From: rpc.From,
 			Data: tx,
 		}, nil
-	}
-}
+	case MessageTypeBlock:
+		block := new(core.Block)
+		if err := block.Decode(core.NewGobBlockDecoder(bytes.NewReader(msg.Data))); err != nil {
+			return nil, err
+		}
+		return &DecodedMessage{
+			From: rpc.From,
+			Data: block,
+		}, nil
+	case MessageTypeStatus:
+		statusMessage := new(StatusMessage)
+		if err := gob.NewDecoder(bytes.NewReader(msg.Data)).Decode(statusMessage); err != nil {
+			return nil, err
+		}
 
-type RPC struct {
-	From    NetAddr
-	Payload io.Reader
+		return &DecodedMessage{
+			From: rpc.From,
+			Data: statusMessage,
+		}, nil
+	case MessageTypeGetStatus:
+
+		return &DecodedMessage{
+			From: rpc.From,
+			Data: &GetStatusMessage{},
+		}, nil
+	}
+
 }
 
 type RPCPRocessor interface {
