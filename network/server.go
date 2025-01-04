ae4648c93ecab3f2f9368617339e79a227002b68
Lukasz Bulinski <lucas.bulinski@gmail.com>
2025-01-04 10:42:36 +0100

    syncing blocks logic

--- network/server.go
+++ network/server.go
@@ -2,6 +2,8 @@ package network
 
 import (
 	"bytes"
+	"encoding/gob"
+	"fmt"
 	"os"
 	"time"
 
@@ -15,6 +17,7 @@ var defaultBlockTime = 5 * time.Second
 
 type ServerOpts struct {
 	ID            string
+	Transport     Transport
 	Logger        log.Logger
 	RPCDecodeFunc RPCDecodeFunc
 	RPCPRocessor  RPCPRocessor
@@ -42,7 +45,7 @@ func NewServer(opts ServerOpts) (*Server, error) {
 	}
 	if opts.Logger == nil {
 		opts.Logger = log.NewLogfmtLogger(os.Stdout)
-		opts.Logger = log.With(opts.Logger, "ID", opts.ID)
+		opts.Logger = log.With(opts.Logger, "addr", opts.Transport.Addr())
 	}
 	chain, err := core.NewBlockChain(opts.Logger, genesisBlock())
 	if err != nil {
@@ -51,7 +54,7 @@ func NewServer(opts ServerOpts) (*Server, error) {
 	s := &Server{
 		ServerOpts: opts,
 		chain:      chain,
-		memPool:    NewTxPool(),
+		memPool:    NewTxPool(1000),
 		// if we are validator we need a private key
 		// if it's simple node we are not going to
 		// sign blocks
@@ -67,6 +70,9 @@ func NewServer(opts ServerOpts) (*Server, error) {
 	if s.isValidator {
 		go s.validatorLoop()
 	}
+
+	s.bootstrapNodes()
+
 	return s, nil
 }
 func (s *Server) Start() {
@@ -83,12 +89,31 @@ free:
 				s.Logger.Log("error", err)
 			}
 			if err := s.ProcessMessage(msg); err != nil {
-				s.Logger.Log("error", err)
+				if err != core.ErrBlockKnown {
+					s.Logger.Log("error", err)
+
+				}
 			}
 		case <-s.quitch:
 			break free
 		}
 	}
+}
+
+func (s *Server) bootstrapNodes() {
+	for _, tr := range s.Transports {
+		if s.Transport.Addr() != tr.Addr() {
+			if err := s.Transport.Connect(tr); err != nil {
+				s.Logger.Log("error", "could not connect to remote", "err", err)
+			}
+			s.Logger.Log("msg", "connect to remote", "we", s.Transport.Addr(), "addr", tr.Addr())
+			if err := s.sendGetStatusMessage(tr); err != nil {
+				s.Logger.Log("error", "sendGetStatusMessage", "err", err)
+			}
+		}
+
+	}
+
 }
 
 func (s *Server) validatorLoop() {
@@ -97,7 +122,6 @@ func (s *Server) validatorLoop() {
 		"msg", "starting validator loop",
 		"blocktime", s.BlockTime,
 	)
-
 	for {
 		<-ticker.C
 		s.createNewBlock()
@@ -108,6 +132,79 @@ func (s *Server) ProcessMessage(msg *DecodedMessage) error {
 	switch t := msg.Data.(type) {
 	case *core.Transaction:
 		return s.processTransaction(t)
+	case *core.Block:
+		return s.processBlock(t)
+	case *GetStatusMessage:
+		s.processGetStatusMessage(msg.From, t)
+	case *StatusMessage:
+		s.processStatusMessage(msg.From, t)
+	case *GetBlocksMessage:
+		s.processGetBlocksMessage(msg.From, t)
+	}
+
+	return nil
+}
+
+func (s *Server) processGetBlocksMessage(from NetAddr, msg *GetBlocksMessage) error {
+	return nil
+}
+
+func (s *Server) processStatusMessage(from NetAddr, data *StatusMessage) error {
+	if data.CurrentHeight <= s.chain.Height() {
+		s.Logger.Log("msg", "cannoct sync block height to low", "outHeight", s.chain.Height(), "thier height", data.CurrentHeight, "addr", from)
+		return nil
+	}
+	// in this case i am sure that node has blocks higher than this
+
+	var (
+		getBlocksMessage = &GetBlocksMessage{
+			From: s.chain.Height(),
+			To:   0,
+		}
+		buf = new(bytes.Buffer)
+	)
+
+	if err := gob.NewEncoder(buf).Encode(getBlocksMessage); err != nil {
+		return err
+	}
+
+	msg := NewMessage(MessageTypeGetBlocks, buf.Bytes())
+
+	return s.Transport.SendMessage(from, msg.Bytes())
+}
+
+func (s *Server) processGetStatusMessage(from NetAddr, data *GetStatusMessage) error {
+	fmt.Printf("received GET status msg %s => %v\n", from, data)
+	statusMessage := &StatusMessage{
+		CurrentHeight: s.chain.Height(),
+		ID:            s.ID,
+	}
+
+	buf := new(bytes.Buffer)
+	if err := gob.NewEncoder(buf).Encode(statusMessage); err != nil {
+		return err
+	}
+
+	msg := NewMessage(MessageTypeStatus, buf.Bytes())
+
+	return s.Transport.SendMessage(from, msg.Bytes())
+
+}
+
+func (s *Server) sendGetStatusMessage(tr Transport) error {
+	var (
+		getStatusMsg = new(GetStatusMessage)
+		buf          = new(bytes.Buffer)
+	)
+
+	if err := gob.NewEncoder(buf).Encode(getStatusMsg); err != nil {
+		return err
+	}
+
+	msg := NewMessage(MessageTypeGetStatus, buf.Bytes())
+	if err := s.Transport.SendMessage(tr.Addr(), msg.Bytes()); err != nil {
+		return err
+
 	}
 	return nil
 }
@@ -121,8 +218,22 @@ func (s *Server) broadcast(msg []byte) error {
 	return nil
 }
 
-func (s *Server) broadcastBlock(b *core.Block) error {
+func (s *Server) processBlock(b *core.Block) error {
+	if err := s.chain.AddBlock(b); err != nil {
+		return err
+	}
+	go s.broadcastBlock(b)
 	return nil
+}
+
+func (s *Server) broadcastBlock(b *core.Block) error {
+	buf := new(bytes.Buffer)
+	if err := b.Encode(core.NewGobBlockEncoder(buf)); err != nil {
+		return err
+	}
+	msg := NewMessage(MessageTypeBlock, buf.Bytes())
+
+	return s.broadcast(msg.Bytes())
 }
 
 func (s *Server) broadcastTx(tx *core.Transaction) error {
@@ -137,7 +248,7 @@ func (s *Server) broadcastTx(tx *core.Transaction) error {
 
 func (s *Server) processTransaction(tx *core.Transaction) error {
 	hash := tx.Hash(core.TxHasher{})
-	if s.memPool.Has(hash) {
+	if s.memPool.Contains(hash) {
 		return nil
 	}
 
@@ -147,15 +258,16 @@ func (s *Server) processTransaction(tx *core.Transaction) error {
 
 	tx.SetFirstSeen(time.Now().UnixNano())
 
-	s.Logger.Log(
-		"msg", "adding new tx mem",
-		"hash", hash,
-		"mempoolLength", s.memPool.Len(),
-	)
+	// s.Logger.Log(
+	// 	"msg", "adding new tx mem",
+	// 	"hash", hash,
+	// 	"mempoolLength", s.memPool.PendingCount(),
+	// )
 
 	go s.broadcastTx(tx)
 
-	return s.memPool.Add(tx)
+	s.memPool.Add(tx)
+	return nil
 }
 
 func (s *Server) createNewBlock() error {
@@ -166,7 +278,7 @@ func (s *Server) createNewBlock() error {
 
 	// for now we are putting everything in block
 	// in reality we can put everything
-	txx := s.memPool.Transactions()
+	txx := s.memPool.Pending()
 
 	block, err := core.NewBlockFromPrevHeader(currentHeader, txx)
 
@@ -182,7 +294,9 @@ func (s *Server) createNewBlock() error {
 		return err
 	}
 
-	s.memPool.Flush()
+	s.memPool.ClearPending()
+
+	go s.broadcastBlock(block)
 
 	return nil
 }
