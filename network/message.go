7aa2d6c85e447c4eec8dfe9ddc07a43f2b4736df
Lukasz Bulinski <lucas.bulinski@gmail.com>
2025-01-05 10:35:36 +0100

    GetBlocks message

--- network/message.go
+++ network/message.go
@@ -1,10 +1,16 @@
 package network
 
+import "github.com/ukibbb/blockchain-go/core"
+
 type GetBlocksMessage struct {
 	// from this height to that height
 	From uint32
 	// if To is 0 maximum block will be returned
 	To uint32
+}
+
+type BlocksMessage struct {
+	Blocks []*core.Block
 }
 
 type GetStatusMessage struct{}
