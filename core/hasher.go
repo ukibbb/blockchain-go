1b8aa117cfff28efde3132f729346c079c6aaf38
Lukasz Bulinski <lucas.bulinski@gmail.com>
2025-01-08 18:18:07 +0100

    hash whole bytes of tx

--- core/hasher.go
+++ core/hasher.go
@@ -1,7 +1,9 @@
 package core
 
 import (
+	"bytes"
 	"crypto/sha256"
+	"encoding/binary"
 
 	"github.com/ukibbb/blockchain-go/types"
 )
@@ -15,11 +17,19 @@ type BlockHasher struct{}
 func (BlockHasher) Hash(b *Header) types.Hash {
 	h := sha256.Sum256(b.Bytes())
 	return types.Hash(h)
-
 }
 
 type TxHasher struct{}
 
+// Hash will hash the whole bytes of the TX no exception.
 func (TxHasher) Hash(tx *Transaction) types.Hash {
-	return types.Hash(sha256.Sum256(tx.Data))
+	buf := new(bytes.Buffer)
+
+	binary.Write(buf, binary.LittleEndian, tx.Data)
+	binary.Write(buf, binary.LittleEndian, tx.To)
+	binary.Write(buf, binary.LittleEndian, tx.Value)
+	binary.Write(buf, binary.LittleEndian, tx.From)
+	binary.Write(buf, binary.LittleEndian, tx.Nonce)
+
+	return types.Hash(sha256.Sum256(buf.Bytes()))
 }
